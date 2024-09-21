c80b298efa38920af91f6d01be96782ff2e91399
raphael <rafavfx1@gmail.com>
2024-09-21 14:05:47 -0300

    slick outside foreach

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 
+   
+
     function slickRender(){
         return new Promise (function (resolve, reject) {
             $.ajax({
                 type: "GET",
-                url: "https://api.thecatapi.com/v1/images/search?limit=5",
+                url: "https://api.thecatapi.com/v1/images/search?limit=10",
                 success: function (response) {
                     $.each(response, function(index, item) {
                         var key = index + 1;
@@ -14,31 +16,32 @@ $(document).ready(function(){
                         cat_img.attr('width', item.width);
                         cat_img.attr('height', item.height);
                         
-                        console.log(`#cat-${key}`)
+                        // console.log(`#cat-${key}`)
                         $(`#cat-${key}`).append(cat_img)
                         
-                        $('.cats').slick({
-                            lazyLoad: 'ondemand',
-                            slidesToScroll: 1,
-                            dots: true,
-                            infinite: true,
-                            slidesToShow: 3,
-                            adaptiveHeight: true
-                        });
-                    
-                        $('.cats').on('swipe', function(event, slick, direction){
-                            console.log('Swipe direction: ' + direction);
-                        });
-                    
-                        $('.cats').on('click', '.slick-prev', function() {
-                            console.log('Arrow previous clicked!');
-                        });
                         
-                        // Captura o clique na seta "próxima"
-                        $('.cats').on('click', '.slick-next', function() {
-                            console.log('Arrow next clicked!');
-                        });
                     })
+                    
+                    $('.cats').slick({
+                        // lazyLoad: 'ondemand',
+                        slidesToScroll: 1,
+                        dots: true,
+                        infinite: true,
+                        slidesToShow: 3,
+                        adaptiveHeight: true
+                    });
+
+                    $('.cats').on('swipe', function(event, slick, direction){
+                        console.log('Swipe direction: ' + direction);
+                    });
+                    
+                    $('.cats').on('click', '.slick-prev', function() {
+                        console.log('Arrow previous clicked!');
+                    });
+                    
+                    $('.cats').on('click', '.slick-next', function() {
+                        console.log('Arrow next clicked!');
+                    });
                     
                 }
             });   
